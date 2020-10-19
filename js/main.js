d815531658f5b45498d1e89e2e066e8b57d19c77
esbqjl <47468327+esbqjl@users.noreply.github.com>
2020-10-19 01:44:13 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,67 +1,68 @@
 $(document).ready (function() {
-  var alreadyFilled = false;
-    var state;
-  async function hi(){ 
- 
-       await fetch('data/state.json')
-  .then(function(resp){
-    return resp.json();
-  })
-  .then(function(data){
-state=data;
-    console.log(data);
-  }
-  );
-
-initDialog();}
- function initDialog() {
-
-      clearDialog();
-      for (var i = 0; i < state.length; i++) {
-          $('.dialog').append('<div>' + state[i].name + '</div>');
-      }
-  }
-  function clearDialog() {
-      $('.dialog').empty();
-  }
-  $('.typeahead input').click(function() {
-      if (!alreadyFilled) {
-          $('.dialog').addClass('open');
-
-      }
-
-  });
-  $('body').on('click', '.dialog > div', function() {
-      $('.typeahead input').val($(this).text()).focus();
-      $('.typeahead .close').addClass('visible');
-      alreadyFilled = true;
-  });
-  $('.typeahead .close').click(function() {
-      alreadyFilled = false;
-      $('.dialog').addClass('open');
-      $('.autocomplete input').val('').focus();
-      $(this).removeClass('visible');
-  });
-
-  function match(str) {
-      str = str.toLowerCase();
-      clearDialog();
-      for (var i = 0; i < state.length; i++) {
-          if (state[i].name.toLowerCase().startsWith(str)) {
-              $('.dialog').append('<div>' + state[i].name + '</div>');
-          }
-      }
-  }
-  $('.typeahead input').on('input', function() {
-      $('.dialog').addClass('open');
-      alreadyFilled = false;
-      match($(this).val());
-  });
-  $('body').click(function(e) {
-      if (!$(e.target).is("input, .close")) {
-          $('.dialog').removeClass('open');
-      }
-  });
-hi();
+    var alreadyFilled = false;
+      var state;
+    async function hi(){ 
+            await fetch('http://api.geonames.org/childrenJSON?geonameId=6252001&username=esbqjl')
+    .then(function(resp){
+      return resp.json();
+    })
+    .then(function(data){
+  state=data;
+      console.log(data);
+    }
+    ); initDialog();}
+   function initDialog() {
+console.log(state.geonames);
+        clearDialog();
+        for (var i = 0; i < state.geonames.length; i++) {
+            $('.dialog').append('<div>' + state.geonames[i].toponymName + '</div>');
+        }
+    }
+    function clearDialog() {
+        $('.dialog').empty();
+    }
+    $('.typeahead input').click(function() {
+        if (!alreadyFilled) {
+            $('.dialog').addClass('open');
   
-});
+        }
+  
+    });
+    $('body').on('click', '.dialog > div', function() {
+        $('.typeahead input').val($(this).text()).focus();
+        $('.typeahead .close').addClass('visible');
+        alreadyFilled = true;
+    });
+    $('.typeahead .close').click(function() {
+        alreadyFilled = false;
+        $('.dialog').addClass('open');
+        $('.typeahead input').val('').focus();
+        $(this).removeClass('visible');
+    });
+  
+    function match(str) {
+        str = str.toLowerCase();
+        clearDialog();
+        for (var i = 0; i < state.geonames.length; i++) {
+                                    
+                                                  
+            if (state.geonames[i].toponymName.toLowerCase().includes(str)) {
+                          
+                $('.dialog').append('<div>' + state.geonames[i].toponymName + '</div>');
+            }
+  }      
+    
+  }
+    $('.typeahead input').on('input', function() {
+        $('.dialog').addClass('open');
+        alreadyFilled = false;
+        match($(this).val());
+    });
+    $('body').click(function(e) {
+        if (!$(e.target).is("input, .close")) {
+            $('.dialog').removeClass('open');
+        }
+    });
+  hi();
+    
+  });
